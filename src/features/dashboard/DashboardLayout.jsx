c3423635a9724a7f7bcdb850bc57236b29092286
Dilnawaz Khan <dilnawaz.khan@yl-solutions.de>
2024-06-23 21:02:39 +0500

    hooks and apis for stays and bookings implemented

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,11 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { isLoading: isBookingsLoading, bookings } = useRecentBookings();
+  const { isLoading: isStaysLoading, stays, confirmedStays } = useRecentStays();
+
+  if (isBookingsLoading || isStaysLoading) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
